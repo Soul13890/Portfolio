13f39e0ed22c810fd447fe02eb9539709a94da51
Florian Badenes <f.badenes@orange.fr>
2025-11-17 17:12:22 +0100

    Petits fixs

--- src/components/Skill/skill.jsx
+++ src/components/Skill/skill.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Skill({icon, name, level}) { 
 
-    const[width, setWidth] = useState(30);
+    const[width, setWidth] = useState(40);
 
     useEffect(() => {
-        let start = 30;
+        let start = 40;
         let end = level;
-        let duration = 1000; // 1 seconde
+        let duration = 1000;
         let startTime = null;
 
         const animate = (timestamp) => {
             if (!startTime) startTime = timestamp;
             const progress = timestamp - startTime;
-
-            // progression entre 0 et 1
+            
             const ratio = Math.min(progress / duration, 1);
 
             const value = start + (end - start) * ratio;
