b4026de1746f531cc3bd689461363571a0a6000f
Florian Badenes <f.badenes@orange.fr>
2025-11-26 16:20:01 +0100

    Fix affichage boutons

--- src/components/Project/project.jsx
+++ src/components/Project/project.jsx
@@ -38,7 +38,7 @@ function Project({id, name, cover, desc, icons, categories, github}) {
                 </div>
                 <div className="flex content-center gap-2">
                     {github != "" && <motion.a href={github} target="_blank" rel="noopener noreferrer" className="flex justify-center content-center gap-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><FaGithub />GitHub</motion.a>}
-                    {(location.pathname === "/" || location.hash.startsWith("#/projects")) && <MotionLink to={`/projects/${id}`} className="flex content-center gap-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>Plus d'infos<IoIosArrowRoundForward /></MotionLink>}
+                    {(location.pathname === "/" || location.pathname === "/#/projects") && <MotionLink to={`/projects/${id}`} className="flex content-center gap-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>Plus d'infos<IoIosArrowRoundForward /></MotionLink>}
                 </div>
                 
             </div>
