62575deed83e4fecefc266b74e8b026c0517e87b
Florian Badenes <f.badenes@orange.fr>
2025-11-26 17:11:22 +0100

    Fix affichage bouton retour

--- src/components/Projects_Intro/projects_intro.jsx
+++ src/components/Projects_Intro/projects_intro.jsx
@@ -1,15 +1,16 @@
 import projects_data from "../../datas/Projects.json";
 import Project from "../Project/project";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { motion } from "motion/react";
 import { IoIosArrowRoundBack } from "react-icons/io";
 
 function Projects_Intro() {
   const MotionLink = motion(Link);
+  const location = useLocation();
 
   return (         
       <section id="my-projects" className='projects-intro-container flex flex-col gap-8 max-w-[1240px] w-full'>
-          {window.location.pathname.includes("/projects") && <MotionLink to={`/`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><IoIosArrowRoundBack />Retour</MotionLink>}
+          {location.pathname === "/projects" && <MotionLink to={`/`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><IoIosArrowRoundBack />Retour</MotionLink>}
           <h2>J'ai travaillé sur...</h2>
           <ul className='projects-list flex flex-col gap-8'>
               {projects_data.map(({id, name, cover, desc, icons, categories, github}) => (              
@@ -26,7 +27,7 @@ function Projects_Intro() {
                 </li>
               ))}
           </ul>
-          {window.location.pathname.includes("/projects") && <MotionLink to={`/`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><IoIosArrowRoundBack />Retour</MotionLink>}  
+          {location.pathname === "/projects" && <MotionLink to={`/`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><IoIosArrowRoundBack />Retour</MotionLink>}  
       </section>   
   )
 }
