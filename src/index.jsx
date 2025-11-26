8c8ff5b448bf6ac42a5393bcbb332e10f0dd0445
Florian Badenes <f.badenes@orange.fr>
2025-11-26 15:47:46 +0100

    Fix reload page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/home'
 import About from './pages/About/about'
 import Projects from './pages/Projects/projects'
