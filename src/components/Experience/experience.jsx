df161e51afd7d4811a68751265158b42996b353a
Florian Badenes <f.badenes@orange.fr>
2025-11-27 13:52:16 +0100

    Fix gap experience

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -4,7 +4,7 @@ import { LuScrollText } from "react-icons/lu";
 
 function Experience({ id, name, cover, establishment, location, year, type, description, skills}) {
     return (
-        <article className="experience-container flex flex-col gap-[30px]">
+        <article className="experience-container flex flex-col gap-4">
             <div className="experience-header flex">
                 <img src={cover} alt="Logo de l'établissement" />
                 <div className="experience-infos flex flex-col gap-2">
