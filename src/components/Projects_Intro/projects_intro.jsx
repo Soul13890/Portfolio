a1c1245562045b640ee051b5ed3b0d90acce8cc3
Florian Badenes <f.badenes@orange.fr>
2025-12-11 12:48:04 +0100

    Ajout gestion highlighted projects

--- src/components/Projects_Intro/projects_intro.jsx
+++ src/components/Projects_Intro/projects_intro.jsx
@@ -2,7 +2,7 @@ import projects_data from "../../datas/Projects.json";
 import Project from "../Project/project";
 import { Link, useLocation } from "react-router-dom";
 import { motion } from "motion/react";
-import { IoIosArrowRoundBack } from "react-icons/io";
+import { IoIosArrowRoundForward, IoIosArrowRoundBack } from "react-icons/io";
 
 function Projects_Intro() {
   const MotionLink = motion(Link);
@@ -13,7 +13,7 @@ function Projects_Intro() {
           {location.pathname === "/projects" && <MotionLink to={`/`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><IoIosArrowRoundBack />Retour</MotionLink>}
           <h2>J'ai travaillé sur...</h2>
           <ul className='projects-list flex flex-col gap-8'>
-              {projects_data.map(({id, name, cover, desc, icons, categories, github}) => (              
+              {location.pathname === "/projects" && projects_data.map(({id, name, cover, desc, icons, categories, github}) => (              
                 <li className="project-item" key={id}>
                     <Project                                                   
                         id={id}
@@ -26,6 +26,24 @@ function Projects_Intro() {
                     />
                 </li>
               ))}
+              {location.pathname === "/" &&
+                projects_data
+                    .filter(project => project.highlighted)      // <= on garde seulement les highlightés
+                    .map(({ id, name, cover, desc, icons, categories, github }) => (
+                    <li className="project-item" key={id}>
+                        <Project
+                        id={id}
+                        name={name}
+                        cover={cover}
+                        desc={desc}
+                        icons={icons}
+                        categories={categories}
+                        github={github}
+                        />
+                    </li>
+                    ))
+                }
+                {location.pathname === "/" &&<MotionLink to={`/projects`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>Autres projets<IoIosArrowRoundForward/></MotionLink>} 
           </ul>
           {location.pathname === "/projects" && <MotionLink to={`/`} className="flex content-center gap-2 w-fit" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}><IoIosArrowRoundBack />Retour</MotionLink>}  
       </section>   
